4994a642109b23191ab6cf2bf32f8015baa7e5ab
Shayna Chambless <shaychamblessshay@gmail.com>
2018-06-25 15:19:12 -0700

    added start text
    start tet

--- textadventure.py
+++ textadventure.py
@@ -1,4 +1,8 @@
-start = ""
+start = '''
+After spending what seems like an eternity looking for the perfect chair in ikea,
+you realize you are lost. You look around and see you are in a strange room with
+a door on you left and your right.
+'''
 print(start)
 
 print("Type 'left' to go left or 'right' to go right.")
@@ -8,6 +12,3 @@ if(user_input == "left"):
     print("You decide to go left and...")
 elif(user_input == "right"):
     print("You decide to go right and...")
-
-
-    #yert    

ce2daf46b7de442c450d9d6bd25785438ef84447
Shayna Chambless <shaychamblessshay@gmail.com>
2018-06-25 14:56:15 -0700

    starter code
    uploaded source code

---
+++ textadventure.py
@@ -0,0 +1,10 @@
+start = ""
+print(start)
+
+print("Type 'left' to go left or 'right' to go right.")
+
+user_input = input()
+if(user_input == "left"):
+    print("You decide to go left and...")
+elif(user_input == "right"):
+    print("You decide to go right and...")    
